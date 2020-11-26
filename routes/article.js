0a506cb44ac9181de44040809cabdb5f2b297ccd
hjdbenny <479925494@qq.com>
2020-11-26 17:34:34 +0800

    更新_id为id

--- routes/article.js
+++ routes/article.js
@@ -1,20 +1,20 @@
-const router = require("koa-router")();
-const moment = require("moment");
-const db = require("../utils/db");
+const router = require('koa-router')();
+const moment = require('moment');
+const db = require('../utils/db');
 
-router.prefix("/api/article");
+router.prefix('/api/article');
 
 // 获取文章列表
-router.get("/getArticleList", async (ctx, next) => {
+router.get('/getArticleList', async (ctx, next) => {
     let pageNumber = +ctx.query.pageNumber;
     let pageSize = +ctx.query.pageSize;
-    let count = await db.collection("articles").count();
+    let count = await db.collection('articles').count();
     let result;
     if (pageSize == 0) {
-        result = await db.collection("articles").get();
+        result = await db.collection('articles').get();
     } else {
         result = await db
-            .collection("articles")
+            .collection('articles')
             .limit(pageSize)
             .skip((pageNumber - 1) * pageSize)
             .get();
@@ -34,11 +34,14 @@ router.get("/getArticleList", async (ctx, next) => {
     };
 });
 // 根据id获取文章
-router.get("/getArticle", async (ctx, next) => {
+router.get('/getArticle', async (ctx, next) => {
     let result = await db
-        .collection("articles")
+        .collection('articles')
         .where({ _id: ctx.query.id })
         .get();
+    result.data.forEach((item) => {
+        item.id = item._id;
+    });
     ctx.body = {
         code: 0,
         message: null,
@@ -46,55 +49,55 @@ router.get("/getArticle", async (ctx, next) => {
     };
 });
 // 新增文章
-router.post("/addArticle", async (ctx, next) => {
+router.post('/addArticle', async (ctx, next) => {
     let data = ctx.request.body;
-    let result = await db.collection("articles").add({
+    let result = await db.collection('articles').add({
         title: data.title,
         content: data.content,
         author: data.author,
-        createTime: moment().format("YYYY-MM-DD HH:mm:ss"),
-        updateTime: moment().format("YYYY-MM-DD HH:mm:ss"),
+        createTime: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updateTime: moment().format('YYYY-MM-DD HH:mm:ss'),
     });
     ctx.body = {
         code: 0,
-        message: "新增成功",
+        message: '新增成功',
         data: result.id,
     };
 });
 // 更新文章
-router.post("/updateArticle", async (ctx, next) => {
+router.post('/updateArticle', async (ctx, next) => {
     let data = ctx.request.body;
     let newData = {};
     for (const key in data) {
         if (data.hasOwnProperty(key)) {
-            if (key != "id") {
+            if (key != 'id') {
                 newData[key] = data[key];
             }
         }
     }
-    newData.updateTime = moment().format("YYYY-MM-DD HH:mm:ss");
+    newData.updateTime = moment().format('YYYY-MM-DD HH:mm:ss');
     let result = await db
-        .collection("articles")
+        .collection('articles')
         .where({ _id: data.id })
         .update(newData);
     ctx.body = {
         code: 0,
-        message: "修改成功",
+        message: '修改成功',
         data: null,
     };
 });
 
 // 根据id删除文章
-router.delete("/deleteArticle", async (ctx, next) => {
+router.delete('/deleteArticle', async (ctx, next) => {
     let data = ctx.request.body;
     let result = await db
-        .collection("articles")
+        .collection('articles')
         .where({ _id: ctx.request.body.id })
         .remove();
 
     ctx.body = {
         code: 0,
-        message: "删除成功",
+        message: '删除成功',
         data: null,
     };
 });
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
-const router = require("koa-router")();
-const db = require("../utils/db");
+const router = require('koa-router')();
+const db = require('../utils/db');
 
-router.prefix("/api/user");
+router.prefix('/api/user');
 
-router.get("/getUserInfo", async (ctx, next) => {
-    let user = await db.collection("users").where({ _id: ctx.query.id }).get();
+router.get('/getUserInfo', async (ctx, next) => {
+    let user = await db.collection('users').where({ _id: ctx.query.id }).get();
     ctx.body = {
         code: 0,
         message: null,
@@ -18,23 +18,23 @@ router.get("/getUserInfo", async (ctx, next) => {
     };
 });
 
-router.post("/updateUserInfo", async (ctx, next) => {
+router.post('/updateUserInfo', async (ctx, next) => {
     let data = ctx.request.body;
     let newData = {};
     for (const key in data) {
         if (data.hasOwnProperty(key)) {
-            if (key != "id") {
+            if (key != 'id') {
                 newData[key] = data[key];
             }
         }
     }
     let user = await db
-        .collection("users")
+        .collection('users')
         .where({ _id: data.id })
         .update(newData);
     ctx.body = {
         code: 0,
-        message: "修改成功",
+        message: '修改成功',
         data: null,
     };
 });
